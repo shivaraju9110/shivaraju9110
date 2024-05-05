3d5ce15d3e54e834820a62e8db18e36ade9ccd6b
shivaraju9110 <r210646@rguktrkv.ac.in>
2024-05-05 11:50:58 +0530

    Add files via upload

---
+++ import numpy.py
@@ -0,0 +1,18 @@
+import numpy as np 
+from matplotlib import pyplot as plt
+t=np.arange(0,1,0.01)
+sin_dict={ 's1':[2,5],
+	   's2':[5,10],
+	   's3':[3,7],
+	   's4':[10,12],
+	   's5':[1,2],}
+k=input('Enter sinusoidal key to generate: ')
+if sin_dict[k]:
+  x=sin_dict[k][0]*np.sin(2*np.pi*sin_dict[k][1]*t)
+  plt.plot(t,x)
+  plt.show()
+  
+"""in mobile phone
+pydroid3 for python and
+anoc octave for octave
+"""
